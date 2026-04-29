79dea2878019d2e91d0ef2eb7f3622388469c177
Gyanendra Singh <128048332+gyanendradev@users.noreply.github.com>
2026-04-30 00:33:25 +0530

    Add: combination-sum-iv - submission-0

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for(int i = 1; i <= target; i++ ){
+            for(int j = 0; j< nums.length; j++){
+                dp[i] = dp[i] + (i - nums[j]>= 0 ? dp[i - nums[j]] : 0);
+            }
+        }
+        return dp[target];
+    }
+}
